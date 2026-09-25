5133ab99a03486372d55612f2971464bce8d8ffa
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-09-25 17:34:51 +0500

    Github Priflie fetch app ogic complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,75 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+const App = () => {
+  const [githubUserName, setGithubUserName] = useState("");
+  const [githubProfileData , setGithubProfileData] = useState("")
 
-function App() {
-  const [count, setCount] = useState(0)
+  const getUserProfile = async () => {
+    try{
+console.log(githubUserName, "githubUserName");
+    const userProfile = await fetch(
+      `https://api.github.com/users/${githubUserName}`,
+    ).then((res) => res.json());
+    console.log(userProfile, "userProfile");
+setGithubProfileData(userProfile)
+    
+    }catch(error){
+        console.log(error);
+        
+    }
+    
+  }; 
+  console.log(githubProfileData , "githubProfileData");
+//   console.log(githubUserName);
+const uiRender = ()=>{
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    return (
+ githubProfileData.message == "Not Found" ?  (
+        <div> <h1>User Not Found</ h1></div> 
+    ) : <div>
+    <img width={400} src={githubProfileData.avatar_url} alt="" />
+          <h1>{githubProfileData.name}</h1>
+    <p>{githubProfileData.bio}</p>
+    <p>{githubProfileData.company}</p>
+    
+    </div> 
+    )
+       
+ 
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+   
 }
+// return (
+    
 
-export default App
+      
+
+// )
+  return (
+    <div>
+
+    {/* Search Container */}
+    <div>
+          <input
+        style={{ width: "200px" }}
+        type="text"
+        placeholder="Enter your github userName"
+        onChange={(e) => {
+          setGithubUserName(e.target.value);
+        }}
+      />
+      <button onClick={getUserProfile}>Search</button>
+    </div>
+
+{/* Card container */}
+
+{
+    uiRender()
+}
+    
+
+    </div>
+  );
+
+    } 
+
+export default App;
